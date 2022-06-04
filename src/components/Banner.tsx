65aa6ac4f8cefce14acfe886ac52c2c32e887728
AliceMenzie <alice.menzie@gmail.com>
2022-06-04 19:03:56 +1000

    Feature: add Banner component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,13 @@
-export default function Banner() {
+import React from "react";
+
+interface BannerProps {
+  children: React.ReactNode;
+}
+
+export default function Banner({ children }: BannerProps) {
   return (
-    <div>Banner</div>
-  )
+    <div className="shadow-custom2 bg-purple text-white w-[327px] h-[88px] flex flex-col justify-center items-center text-center rounded-md px-6">
+      {children}
+    </div>
+  );
 }
