f7073f883f7b3e7ec651672de142a5f5eb66ae33
AliceMenzie <alice.menzie@gmail.com>
2022-06-04 19:03:35 +1000

    Config: add custom boxShadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,11 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins"],
       },
+      boxShadow: {
+        custom:
+          "rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;",
+        custom2: "rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;",
+      },
     },
   },
   plugins: [],
