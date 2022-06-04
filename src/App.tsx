b4c0e6a24cb8a664a568edfc89a19bad6c6eb2ef
AliceMenzie <alice.menzie@gmail.com>
2022-06-04 18:11:30 +1000

    Feature: add styles to app and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import Banner from "./components/Banner";
 
 import Form from "./components/Form";
 
 function App() {
   return (
-    <div className="bg-red">
+    <div className="font-poppins bg-red flex flex-col items-center justify-center h-full py-20">
+      <header className="text-center text-white py-5">
+        <h1 className="text-2xl font-bold mx-10 mb-4">
+          Learn to code by watching others
+        </h1>
+        <p>
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.
+        </p>
+      </header>
       <main className="">
+        <Banner>
+          <p>
+            <span>Try it free 7 days</span> then $20/mo. thereafter
+          </p>
+        </Banner>
         <Form />
       </main>
     </div>
