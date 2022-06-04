0ff67796e2674c05890764dc6860a3a1fca0a4fb
AliceMenzie <alice.menzie@gmail.com>
2022-06-04 17:55:48 +1000

    Feature: add form mobile style and validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+import iconError from "../assets/images/icon-error.svg";
 
 type Inputs = {
   firstName: string;
@@ -19,41 +20,93 @@ export default function Form() {
 
   return (
     <form
-      className="h-[442px] w-[327px] bg-white flex flex-col items-center justify-around rounded-lg py-3"
+      className="font-poppins h-[442px] w-[327px] bg-white flex flex-col items-center justify-around rounded-lg py-3"
       onSubmit={handleSubmit(onSubmit)}
     >
-      <input
-        className="border text-grey capitalize font-bold w-[279px] h-[56px] rounded-md pl-[14px]"
-        placeholder="first name"
-        {...register("firstName", { required: true })}
-      />
-      {errors.firstName && <span>This field is required</span>}
-      <input
-        className="border text-grey capitalize font-bold w-[279px] h-[56px] rounded-md pl-[14px]"
-        placeholder="Last Name"
-        {...register("lastName", { required: true })}
-      />
-      {errors.lastName && <span>This field is required</span>}
-      <input
-        className="border text-grey capitalize font-bold w-[279px] h-[56px] rounded-md pl-[14px]"
-        placeholder="email address"
-        {...register("email", { required: true })}
-      />
-      {errors.email && <span>This field is required</span>}
-      <input
-        className="border text-grey capitalize font-bold w-[279px] h-[56px] rounded-md pl-[14px]"
-        placeholder="password"
-        type="password"
-        {...register("password", { required: true })}
-      />
-      {errors.password && <span>This field is required</span>}
+      <div className="relative flex justify-center items-center flex-col">
+        <input
+          className={`border text-grey placeholder:capitalize placeholder:font-bold w-[279px] h-[56px] rounded-md pl-[14px] ${
+            errors.firstName && "border border-red text-red"
+          }`}
+          placeholder="first name"
+          {...register("firstName", { required: true })}
+        />
+        {errors.firstName && (
+          <img className="absolute top-4 right-3" src={iconError} alt="error" />
+        )}
+        {errors.firstName && (
+          <span className="text-red text-xs font-bold border-red self-end">
+            First Name connot be empty
+          </span>
+        )}
+      </div>
+      <div className="relative flex justify-center items-center flex-col">
+        <input
+          className={`border text-grey capitalize font-bold w-[279px] h-[56px] rounded-md pl-[14px] ${
+            errors.lastName && "border border-red text-red"
+          }`}
+          placeholder="Last Name"
+          {...register("lastName", { required: true })}
+        />
+        {errors.lastName && (
+          <img className="absolute top-4 right-3" src={iconError} alt="error" />
+        )}
+        {errors.lastName && (
+          <span
+            className="text-red text-xs font-bold border-red self-end
+        "
+          >
+            Last Name cannot be empty
+          </span>
+        )}
+      </div>
+      <div className="relative flex justify-center items-center flex-col">
+        <input
+          className={`border text-grey placeholder:capitalize placeholder:font-bold w-[279px] h-[56px] rounded-md pl-[14px] ${
+            errors.email && "border border-red text-red"
+          }`}
+          placeholder="email address"
+          {...register("email", {
+            required: true,
+            pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+          })}
+        />
+        {errors.email && (
+          <img className="absolute top-4 right-3" src={iconError} alt="error" />
+        )}
+        {errors.email && (
+          <span className="text-red text-xs font-bold border-red self-end">
+            Email cannot be empty
+          </span>
+        )}
+      </div>
+      <div className="relative flex justify-center items-center flex-col">
+        <input
+          className={`border text-grey placeholder:capitalize placeholder:font-bold w-[279px] h-[56px] rounded-md pl-[14px] ${
+            errors.password && "border border-red text-red"
+          }`}
+          placeholder="password"
+          type="password"
+          {...register("password", {
+            required: true,
+          })}
+        />
+        {errors.password && (
+          <img className="absolute top-4 right-3" src={iconError} alt="error" />
+        )}
+        {errors.password && (
+          <span className="text-red text-xs font-bold border-red self-end">
+            Password cannot be empty
+          </span>
+        )}
+      </div>
 
       <input
-        className="bg-green text-white uppercase w-[279px] h-[56px] rounded-md"
+        className="font-poppins font-semibold bg-green text-white uppercase w-[279px] h-[56px] rounded-md "
         type="submit"
         value="Claim your free trial"
       />
-      <p className="text-center text-greyBlue">
+      <p className="font-poppins text-center text-greyBlue text-xs font-semibold mx-10">
         By clicking the button, you are agreeing to our {/* <span> */}
         <a className="text-red font-bold" href="###">
           Terms and Services
