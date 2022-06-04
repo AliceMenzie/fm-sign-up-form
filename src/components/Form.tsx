136e98ecfc76c89941600c00606a53ca4e04f4d9
AliceMenzie <alice.menzie@gmail.com>
2022-06-04 13:32:33 +1000

    Styles:  add form styles

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -16,21 +16,50 @@ export default function Form() {
     formState: { errors },
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("firstName", { required: true })} />
+    <form
+      className="h-[442px] w-[327px] bg-white flex flex-col items-center justify-around rounded-lg py-3"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <input
+        className="border text-grey capitalize font-bold w-[279px] h-[56px] rounded-md pl-[14px]"
+        placeholder="first name"
+        {...register("firstName", { required: true })}
+      />
       {errors.firstName && <span>This field is required</span>}
-      <input {...register("lastName", { required: true })} />
+      <input
+        className="border text-grey capitalize font-bold w-[279px] h-[56px] rounded-md pl-[14px]"
+        placeholder="Last Name"
+        {...register("lastName", { required: true })}
+      />
       {errors.lastName && <span>This field is required</span>}
-      <input {...register("email", { required: true })} />
+      <input
+        className="border text-grey capitalize font-bold w-[279px] h-[56px] rounded-md pl-[14px]"
+        placeholder="email address"
+        {...register("email", { required: true })}
+      />
       {errors.email && <span>This field is required</span>}
-      <input {...register("password", { required: true })} />
+      <input
+        className="border text-grey capitalize font-bold w-[279px] h-[56px] rounded-md pl-[14px]"
+        placeholder="password"
+        type="password"
+        {...register("password", { required: true })}
+      />
       {errors.password && <span>This field is required</span>}
 
-      <input type="submit" value="Claim your free trial" />
-      <span>
-        By clicking the button, you are agreeing to our Terms and Services
-      </span>
+      <input
+        className="bg-green text-white uppercase w-[279px] h-[56px] rounded-md"
+        type="submit"
+        value="Claim your free trial"
+      />
+      <p className="text-center text-greyBlue">
+        By clicking the button, you are agreeing to our {/* <span> */}
+        <a className="text-red font-bold" href="###">
+          Terms and Services
+        </a>
+        {/* </span> */}
+      </p>
     </form>
   );
 }
